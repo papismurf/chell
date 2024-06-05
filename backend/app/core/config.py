63dc2e1a0c2cbfd638aa8ef6dbfe43f22b21be61
dbpython <33459750+papismurf@users.noreply.github.com>
2024-06-05 16:43:19 +0100

    Update config.py
    change http to string in dsn settings variable

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,6 @@ from typing import Annotated, Any, Literal
 from pydantic import (
     AnyUrl,
     BeforeValidator,
-    HttpUrl,
     PostgresDsn,
     computed_field,
     model_validator,
@@ -47,7 +46,7 @@ class Settings(BaseSettings):
     ] = []
 
     PROJECT_NAME: str
-    SENTRY_DSN: HttpUrl | None = None
+    SENTRY_DSN: str | None = None
     POSTGRES_SERVER: str
     POSTGRES_PORT: int = 5432
     POSTGRES_USER: str
